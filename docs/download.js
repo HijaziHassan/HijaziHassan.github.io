4f140ec4149ad677584b932a46004bd8a7bb4132
Hassan HIJAZI <hejaziihassan@gmail.com>
2024-08-27 00:03:02 +0200

    cv download button

--- docs/download.js
+++ docs/download.js
@@ -1,6 +1,6 @@
 // download.js
 document.getElementById('download-cv').addEventListener('click', function() {
-    const cvUrl = 'cv-2024.pdf'; 
+    const cvUrl = './docs/cv-2024.pdf'; 
     const link = document.createElement('a');
     link.href = cvUrl;
     link.download = 'cv-2024.pdf'; 
