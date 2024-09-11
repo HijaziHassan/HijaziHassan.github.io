3cfd3b00cb302a1dc65aecad2b0bc623caf69620
Hassan HIJAZI <hejaziihassan@gmail.com>
2024-09-11 21:58:59 +0200

    open cv (updated) in a new page as pdf, rather than directing to the github page where it is shown embedded.

--- docs/download.js
+++
@@ -1,8 +0,0 @@
-// download.js
-document.getElementById('download-cv').addEventListener('click', function() {
-    const cvUrl = './docs/cv-2024.pdf'; 
-    const link = document.createElement('a');
-    link.href = cvUrl;
-    link.download = 'cv-2024.pdf'; 
-    link.click();
-});
